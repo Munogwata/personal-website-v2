a66df812d42ce6431aef0c0c74219ddb847d62bf
Munozovepi Gwata <muno.gwata@gmail.com>
2022-02-18 20:28:20 +0200

    added elements to navigation menu and made sure that it is responsive, need to add links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/universal-components/nav-bar/nav-bar.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+//material ui imports//
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+
+    //Material UI 
+    MatToolbarModule,
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
